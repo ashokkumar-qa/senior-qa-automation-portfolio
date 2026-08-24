b553df432e86b1b7226d06027354e2a9ae6517dd
Ashok Kumar <ashokkumar.qaautomation@gmail.com>
2026-08-24 15:13:46 +0800

    Set fixed browser viewport for CI

--- selenium-java-framework/src/test/java/base/BaseTest.java
+++ selenium-java-framework/src/test/java/base/BaseTest.java
@@ -1,6 +1,7 @@
 package base;
 
 import driver.DriverFactory;
+import org.openqa.selenium.Dimension;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
@@ -9,12 +10,16 @@ import org.testng.annotations.Parameters;
 public class BaseTest {
 
     // Initializes a separate WebDriver instance before each test method
-    @Parameters("browser")
     @BeforeMethod
+    @Parameters("browser")
     public void setUp(@Optional("chrome") String browser) {
-        DriverFactory.initDriver(browser);
-        DriverFactory.getDriver().manage().window().maximize();
 
+        DriverFactory.initDriver(browser);
+
+        DriverFactory.getDriver()
+                .manage()
+                .window()
+                .setSize(new Dimension(1920, 1080));
     }
 
     // Terminates the WebDriver session after each test method
